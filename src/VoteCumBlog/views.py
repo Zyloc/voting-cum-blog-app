57c22574d3a55213442496f9694f24d8e84f2fda
zyloc <sswarnkar13@gmail.com>
2016-05-28 20:50:37 +0530

    app(VoteCumBlog) has been created

---
+++ src/VoteCumBlog/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+
+class Home(View):
+	def get(self,request):
+		return HttpResponse('Hello welcome newbie!!')
